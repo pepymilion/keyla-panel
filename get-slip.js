ecf8d74d8b5de0a68f1700a5e4c8a1740ed41864
pepymilion <hengkip509@gmail.com>
2025-05-02 19:25:13 +0700

    Update get-slip.js

--- get-slip.js
+++ get-slip.js
@@ -1,7 +1,7 @@
 import { createClient } from './supabase.js';
 
 const supabaseUrl = 'https://mfoiyruematmlkphxvii.supabase.co';
-const supabaseKey = 'eyJhbGciOi...'; // samain kaya yang kamu pakai
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1mb2l5cnVlbWF0bWxrcGh4dmlpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDYxNzEyMTgsImV4cCI6MjA2MTc0NzIxOH0.ho-DpN7naxQeo60l-1vSSUzMUxFUiUSE9fAVvigijAI';
 
 const supabase = createClient(supabaseUrl, supabaseKey);
 
@@ -16,5 +16,5 @@ export async function getAllSlip() {
     return [];
   }
 
-  return data; // <== WAJIB ADA INI!
+  return data;
 }
