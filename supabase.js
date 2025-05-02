c44b47b278124a9d517baf29ad46b2d37fc53b66
pepymilion <hengkip509@gmail.com>
2025-05-02 15:48:57 +0700

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,4 +1,4 @@
-import { createClient } from '@supabase/supabase-js'
+import { createClient } from 'https://esm.sh/@supabase/supabase-js'
 
 const supabaseUrl = 'https://mfoiyruematmlkphxvii.supabase.co'
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1mb2l5cnVlbWF0bWxrcGh4dmlpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDYxNzEyMTgsImV4cCI6MjA2MTc0NzIxOH0.ho-DpN7naxQeo60l-1vSSUzMUxFUiUSE9fAVvigijAI'
