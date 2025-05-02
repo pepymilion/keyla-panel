d9ad83fdcbd1f95ae7c8e952d272e7d57ff3e9d0
pepymilion <hengkip509@gmail.com>
2025-05-02 19:50:41 +0700

    Fix slip mingguan loading

--- get-slip.js
+++ get-slip.js
@@ -1,9 +1,9 @@
-import { createClient } from './supabase.js';
+import { createClient } from 'https://esm.sh/@supabase/supabase-js'
 
-const supabaseUrl = 'https://mfoiyruematmlkphxvii.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1mb2l5cnVlbWF0bWxrcGh4dmlpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDYxNzEyMTgsImV4cCI6MjA2MTc0NzIxOH0.ho-DpN7naxQeo60l-1vSSUzMUxFUiUSE9fAVvigijAI';
+const supabaseUrl = 'https://mfoiyruematmlkphxvii.supabase.co'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1mb2l5cnVlbWF0bWxrcGh4dmlpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDYxNzEyMTgsImV4cCI6MjA2MTc0NzIxOH0.ho-DpN7naxQeo60l-1vSSUzMUxFUiUSE9fAVvigijAI'
 
-const supabase = createClient(supabaseUrl, supabaseKey);
+const supabase = createClient(supabaseUrl, supabaseKey)
 
 export async function getAllSlip() {
   const { data, error } = await supabase
